53f5c838acd15cd03725c22168a9688804380ecd
Aiden <aidenisaac08@gmail.com>
2026-03-24 15:29:37 +0800

    fix: persist uploads volume and exclude from deploy

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,15 @@
 REMOTE_USER="aiden"
 REMOTE_HOST="100.111.229.55"
 REMOTE_PATH="~/cube/cube-stats"
-LOCAL_PATH="/home/aiden/Projects/cube-stats"
+LOCAL_PATH="/home/aiden/Projects/host-cube-stats"
 
 echo "🚀 Deploying Cube Stats to $REMOTE_USER@$REMOTE_HOST..."
 
 # Sync files, excluding data directory and node_modules
-rsync -avz --progress \
+# Added --delete to remove old files on the remote that no longer exist locally
+rsync -avz --delete --progress \
     --exclude 'data/' \
+    --exclude 'uploads/' \
     --exclude 'node_modules/' \
     --exclude '.git/' \
     --exclude 'package-lock.json' \
